badfcda668a0289d5ec64297121f0e03bb1e89cf
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-06-06 00:38:48 -0600

    correccion de error al guardar datos en la tabla detalle_clientes

--- clientes.sql
+++ clientes.sql
@@ -1,7 +1,6 @@
 create table clientes(
     cliente_id SERIAL not null,
     cliente_nombre VARCHAR(50) not null,
-    cliente_apellido VARCHAR(50) not null,
     cliente_nit VARCHAR(50) not null,
     cliente_situacion char (1) DEFAULT '1',
     primary key (cliente_id)
